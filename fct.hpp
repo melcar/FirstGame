f28d61fefaa47672b4ce95f16317ff7dbb495a0f
Marcel <marcelbeltus@hotmail.com>
2016-09-23 16:39:54 +0200

    few classes added

---
+++ fct.hpp
@@ -0,0 +1,18 @@
+#ifndef FCT_H
+#define FCT_H
+class Game{
+public:
+  class Window{
+    public:
+      class Background{
+        public:
+        private:
+      };
+      class Character{
+        public:
+        private:
+      };
+    private:
+  };
+};
+#endif
