467fbef53172ac0b454d7b898e0fac8e97edac94
Marcel <marcelbeltus@hotmail.com>
2016-09-24 14:28:30 +0200

    window class

--- fct.cpp
+++ fct.cpp
@@ -1,5 +1,20 @@
 #include "fct.hpp"
 
-Game::Game(std::string Title){
-  
+Game::Game(const char *Title){
+  Game::_Title = Title;
 };
+Game::~Game(){};
+
+const char *Game::GetTitle(){
+return Game::_Title;
+};
+
+Game::Window::Window(int width, int height, const char * title){
+  Game::Window::_Width = width;
+  Game::Window::_Height = height;
+  SDL_Init(SDL_INIT_VIDEO);
+  Game::Window::_Window = SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_RESIZABLE);
+}
+Game::Window::~Window(){
+  SDL_DestroyWindow(Game::Window::GetWindow());
+}
--- fct.hpp
+++ fct.hpp
@@ -6,20 +6,31 @@
 
 class Game{
   public:
-    Game(std::string Title);
+    Game(const char *Title);
+    ~Game();
+    const char *GetTitle();
+  private:
+    const char *_Title;
     class Window{
       public:
-        Window(int width, int height);
+        Window(int width, int height, const char * Title);
+        ~Window();
+        SDL_Window *GetWindow();
+      private:
+        int _Width; int _Height;
+        SDL_Window *_Window;
         class Background{
           public:
+            Background();
+            ~Background();
           private:
         };
         class Character{
           public:
+            Character();
+            ~Character();
           private:
-        };
-      private:
-        void Makewindows(){}
+      };
     };
   };
 #endif
