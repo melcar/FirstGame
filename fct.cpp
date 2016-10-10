e8d0fadca2f4cf842ee9c3c4cb85118261a7dc96
Marcel <marcelbeltus@hotmail.com>
2016-10-10 13:45:04 +0200

    only making 2 classes

--- fct.cpp
+++ fct.cpp
@@ -1,42 +1,5 @@
 #include "fct.hpp"
 
-Game::Game(){
-  //initiate the Game
-};
-Game::~Game(){};
-
-void Game::GameStart(){
-  Game::_Window.CreateWindow(Game::width, Game::height, Game::Title);
-}
-
-void Game::GameQuit(){
-}
-
-Window::Window(){
-
-}
-
-void Window::CreateWindow(int width, int height, const char * Title){
-  //create and SDL Window
-  Window::_Width = width;
-  Window::_Height = height;
-  SDL_Init(SDL_INIT_VIDEO);
-  Window::_Window = SDL_CreateWindow(Title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_RESIZABLE);
-  Window::_Renderer =  SDL_CreateRenderer(Window::_Window, -1, SDL_RENDERER_TARGETTEXTURE);
-}
-
-void Window::CreateSprite(){
-
-}
-void Window::RefreshWindow(){
-
-}
-Window::~Window(){
-}
-void Window::DeleteWindow(){
-  SDL_DestroyWindow(Window::_Window);
-}
-
 Background::Background(){}
 Background::~Background(){}
 Sprite::Sprite(){}
--- fct.hpp
+++ fct.hpp
@@ -22,35 +22,6 @@
 
 };
 
-class Window{
-  public:
-    Window();
-    void CreateWindow(int width, int height, const char * Title);
-    ~Window();
-    void DeleteWindow();
-    void CreateSprite();
-    void RefreshWindow();
 
-  private:
-    Background _Backgound;
-    Sprite _Sprite1;
-    int _Width; int _Height;
-    SDL_Window *_Window;
-    SDL_Renderer *_Renderer;
-};
 
-class Game{
-  public:
-    Game();
-    ~Game();
-    void GameStart();
-    void GameQuit();
-  private:
-    Window _Window;
-    int width ;
-    int height ;
-    const char * Title ;
-    const char * ImgBackground;
-    const char * ImgSprite;
-  };
 #endif
