e592126a6a8fc1c06f9c7087a3d1c399cfef0a27
Marcel <marcelbeltus@hotmail.com>
2016-10-11 14:36:15 +0200

    start physique

--- Game.cpp
+++ Game.cpp
@@ -8,10 +8,6 @@ int main(){
   int width = 640, height = 480;
   SDL_Texture *Perso = NULL;
   int x , y;
-  SDL_Texture *Text;
-  long int Time =0;
-  std::string str_Time;
-  const char *char_Time ;
 
 
   SDL_Init(SDL_INIT_VIDEO);
@@ -27,58 +23,28 @@ int main(){
 
       Perso = SDL_CreateTextureFromSurface(renderer, SDL_LoadBMP("img/perso.bmp"));
 
-      TTF_Init();
-      //SDL_Surface *TTF_RenderText_Solid(TTF_Font *font, const char *text, SDL_Color fg);
-      TTF_Font *font = TTF_OpenFont("font/3Dventure.ttf", 65);
-      SDL_Color Color = {20, 100, 30};
-
-
-      //Text = SDL_CreateTextureFromSurface(renderer,TTF_RenderText_Solid(font, "222.", Color));
 
       SDL_Rect srcrect = { 0,0,400,400};
       x = 150;
       y = 150;
       //SDL_Rect dstrect = { x,y, 64, 64 };
-      int start_Time = SDL_GetTicks();
       while (1) {
        SDL_Event event;
        if (SDL_PollEvent(&event)) {
          if (event.type == SDL_QUIT){
            break;
          }
-         else if (event.type == SDL_KEYDOWN){
-           if( event.key.keysym.sym == SDLK_a){
-           break;
-         }
-         else if( event.key.keysym.sym == SDLK_z){
-           y -= 5;
-         }
-         else if( event.key.keysym.sym == SDLK_q){
-           x -= 5;
-         }
-         else if( event.key.keysym.sym == SDLK_s){
-           y += 5 ;
-         }
-         else if( event.key.keysym.sym == SDLK_d){
-           x += 5;
-         }
+
        }
-     }
-           Time = (SDL_GetTicks() - start_Time);
-           str_Time = std::to_string(Time);
-           char_Time = str_Time.c_str();
-           Text = SDL_CreateTextureFromSurface(renderer,TTF_RenderText_Solid(font, char_Time, Color));
+
+
            SDL_Rect dstrect = { x,y, 64, 64 };
            SDL_RenderClear(renderer);
            SDL_RenderCopy(renderer, Background, NULL, NULL);
-           SDL_RenderCopy(renderer,Text, NULL, NULL);
            SDL_RenderCopy(renderer,Perso,&srcrect, &dstrect);
            SDL_RenderPresent(renderer);
-           SDL_DestroyTexture(Text);
          }
-     TTF_CloseFont(font);
-     TTF_Quit();
-     SDL_DestroyTexture(Text);
+
      SDL_DestroyTexture(Background);
      SDL_DestroyTexture(Perso);
      SDL_DestroyRenderer(renderer);
--- fct.hpp
+++ fct.hpp
@@ -1,27 +1,8 @@
 #ifndef FCT_H
 #define FCT_H
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>//have tet in sdl
 #include <iostream>
 #include <string>
 
 
-  class Background{
-    public:
-      Background(SDL_Renderer *Renderer, const  char *file);
-      ~Background();
-      SDL_Texture * Get_Background();
-    private:
-      SDL_Texture* _texture;
-      };
-
-  class Sprite{
-    public:
-      Sprite();
-      ~Sprite();
-      void animation(){};
-    private:
-      int posX;
-      int posY;
-};
 #endif
