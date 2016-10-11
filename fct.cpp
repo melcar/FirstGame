72ef63ac995de2d11bb2d9591f5f3a87c9f24aec
Marcel <marcelbeltus@hotmail.com>
2016-10-11 14:30:47 +0200

    fct reseted"

--- fct.cpp
+++ fct.cpp
@@ -1,11 +1,1 @@
 #include "fct.hpp"
-
-Background::Background(SDL_Renderer *Renderer, const  char *file){
-  Background::_texture = SDL_CreateTextureFromSurface(Renderer, SDL_LoadBMP(file));
-}
-Background::~Background(){}
-SDL_Texture *Background::Get_Background(){
-  return Background::_texture;
-}
-Sprite::Sprite(){}
-Sprite::~Sprite(){}
--- fct.hpp
+++ fct.hpp
@@ -6,22 +6,4 @@
 #include <string>
 
 
-  class Background{
-    public:
-      Background(SDL_Renderer *Renderer, const  char *file);
-      ~Background();
-      SDL_Texture * Get_Background();
-    private:
-      SDL_Texture* _texture;
-      };
-
-  class Sprite{
-    public:
-      Sprite();
-      ~Sprite();
-      void animation(){};
-    private:
-      int posX;
-      int posY;
-};
 #endif
